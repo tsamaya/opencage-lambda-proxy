15b3d87bea8d46d0433f7aa97a5c593f486d9caf
Arnaud Ferrand <arferrand@gmail.com>
2020-12-26 18:59:15 +0100

    uses HTTP API and fix unit test vs real world error wrapping

--- handler.js
+++ handler.js
@@ -4,20 +4,26 @@ module.exports.geocode = async (event) => {
   try {
     const result = await geocode(event.queryStringParameters);
     // you might want to log result
-    // console.log(result);
+    // console.log('API result is ', { result });
     return {
       statusCode: 200,
       body: JSON.stringify(result),
     };
   } catch (error) {
     // you might want to log errors
-    // console.log(error);
+    // console.log('Catched error is ', { error });
+    //
+    // Jest fetch wraps response in a response object, while real API
+    // returns directly response into an object containing status
+    const statusCode = error.response.status.code || error.status.code;
+    const message = error.response.status.message || error.status.message;
+    const result = {
+      code: statusCode,
+      message,
+    };
     return {
-      statusCode: error.status.code,
-      body: JSON.stringify({
-        code: error.status.code,
-        message: error.status.message,
-      }),
+      statusCode,
+      body: JSON.stringify(result),
     };
   }
 };
