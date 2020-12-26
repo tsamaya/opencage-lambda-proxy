fb45689af13aa65564bc4226c91b71cf1100e42e
Arnaud Ferrand <arferrand@gmail.com>
2020-12-26 16:47:28 +0100

    removes hello handler and comment logs

--- handler.js
+++ handler.js
@@ -1,30 +1,17 @@
 const { geocode } = require('opencage-api-client');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
-
 module.exports.geocode = async (event) => {
   try {
     const result = await geocode(event.queryStringParameters);
+    // you might want to log result
+    // console.log(result);
     return {
       statusCode: 200,
       body: JSON.stringify(result),
     };
   } catch (error) {
-    // log error message ? and success above ?
+    // you might want to log errors
     // console.log(error);
-
     return {
       statusCode: error.status.code,
       body: JSON.stringify({
